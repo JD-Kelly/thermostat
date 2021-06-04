65dd7f1cf617c39f9af13b4f3551c7753091a1b2
“Jonny <jonny.kelly192@gmail.com>
2021-06-04 15:14:48 +0100

    rest of features built

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -25,6 +25,65 @@ describe('Thermostat', () => {
     expect(thermostat.getCurrentTemperature()).toEqual(10);
   });
   it('has Power Saving Mode set to default', () => {
-    expect(thermostat.isPowerSavingMode()).toBe(true);
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+  it('can turn PSM off', () => {
+    thermostat.turnPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+  it('can turn PSM back on', () => {
+    thermostat.turnPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+    thermostat.turnPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+  describe('when power saving mode is on', () => {
+    it('has a maximum temperature of 25 degrees', () => {
+      for (let i = 0; i < 6; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+  });
+  describe('when power saving mode is off', () => {
+    it('has a maximum temperature of 32 degress', () => {
+      thermostat.turnPowerSavingModeOff();
+      for (let i = 0; i < 13; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(32);
+    })
+  });
+  it('can be reset to default temperature', () => {
+    for (let i = 0; i < 6; i++) {
+      thermostat.up();
+    }
+    thermostat.resetTemperature();
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+  describe('displaying energy usage', () => {
+    describe('when the temperature is below 18 degrees', () => {
+      it('returns low-usage', () => {
+        for (let i = 0; i < 3; i++) {
+          thermostat.down();
+        }
+        expect(thermostat.energyUsage()).toEqual('low-usage');
+      });
+    });
+    describe('when the temperature is between 18 and 25', () => {
+      it('returns medium-usage', () => {
+        expect(thermostat.energyUsage()).toEqual('medium-usage');
+      });
+    });
+    describe('when the temperature is anything else', () => {
+      it('returns high-usage', () => {
+        thermostat.powerSavingMode = false;
+        for (let i = 0; i < 6; i++) {
+          thermostat.up();
+        }
+        expect(thermostat.energyUsage()).toEqual('high-usage');
+      });
+    });
   });
 });
+
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,15 +1,25 @@
+'use strict';
+
 class Thermostat {
   
   constructor() {
-    this.temperature = 20;
     this.MINIMUM_TEMPERATURE = 10;
     this.powerSavingMode = true;
+    this.MAXIMUM_TEMPERATURE_PSM_ON = 25;
+    this.MAXIMUM_TEMPERATURE_PSM_OFF = 32;
+    this.DEFAULT_TEMPERATURE = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
   }
   getCurrentTemperature() {
     return this.temperature;
   }
   up() {
-    return this.temperature += 1
+    if (this.isMaximumTemperature()) {
+      return;
+    }
+    this.temperature += 1
   }
   down() {
     if (this.isMinimumTemperature()) {
@@ -20,7 +30,31 @@ class Thermostat {
   isMinimumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
   }
-  isPowerSavingMode() {
+  isMaximumTemperature() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.MAXIMUM_TEMPERATURE_PSM_OFF;
+    }
+    return this.temperature === this.MAXIMUM_TEMPERATURE_PSM_ON;
+  }
+  isPowerSavingModeOn() {
     return this.powerSavingMode === true;
+  }
+  turnPowerSavingModeOff() {
+    return this.powerSavingMode = false;
+  }
+  turnPowerSavingModeOn() {
+    return this.powerSavingMode = true;
+  }
+  resetTemperature() {
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  }
+  energyUsage() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    }
+    if (this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
+      return 'medium-usage';
+    }
+      return 'high-usage';
   }
 }
